5bc734fcc57e0c33d8a662624a633faf769c6534
JGILINGER7 <jacobgilinger@gmail.com>
2022-10-19 10:15:55 -0400

    added txt for analysis

--- pypoll.py
+++ pypoll.py
@@ -5,12 +5,28 @@
 #4. The total number of votes each candidate won
 #5. The winner of the election based on popular vote.
 
+import csv
+import os
 # Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
-
-# Open the election results and read the file
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
+    # Print the file object.
      print(election_data)
+
+ # Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+ # Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+     txt_file.write("Arapahoe, ")
+     txt_file.write("Denver, ")
+     txt_file.write("Jefferson, ")
      
