baa944ca43e640f5dcd2e6aad252db66755583d3
JGILINGER7 <jacobgilinger@gmail.com>
2022-10-19 09:46:19 -0400

    connected pypoll to election data

--- pypoll.py
+++ pypoll.py
@@ -5,4 +5,12 @@
 #4. The total number of votes each candidate won
 #5. The winner of the election based on popular vote.
 
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
 
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+     
